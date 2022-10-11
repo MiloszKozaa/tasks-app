ad363bc03a158b1a05d8009d0805bad37f2f17f4
Amen't <milosz.koza12@gmail.com>
2022-10-11 23:57:58 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, inputSet] = useState('');
+  const [list, listSet] = useState([
+    {
+      id: 0,
+      text: '',
+    },
+  ]);
+
+  const addToDo = (toDo: string) => {
+    const newToDo = {
+      id: Math.random(),
+      text: toDo,
+    };
+    listSet([...list, newToDo]);
+    console.log(...list);
+    console.log(newToDo);
+  };
+
+  const onEnterPress = (event: any) => {
+    if (event.key === 'Enter') {
+      addToDo(input);
+      inputSet('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        type='text'
+        value={input}
+        onChange={e => {
+          inputSet(e.target.value);
+        }}
+        onKeyPress={onEnterPress}
+      />
+      {list.length - 1}
+      <ul>
+        {list.map((task: any) =>
+          task.text !== '' ? <li key={task.id}>{task.text}</li> : null
+        )}
+      </ul>
     </div>
   );
 }
